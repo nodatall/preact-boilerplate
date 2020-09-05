fda2f6413efa75cd98ae31b7a7edbd4626b862fa
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-09-05 12:25:29 -0700

    updates webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,43 @@
 const path = require('path')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
-module.exports = {
-  entry: ['@babel/polyfill', './src/index.js'],
-  output: {
-    filename: 'bundle.js'
+const ROOT = path.resolve(__dirname, '.')
+const srcPath = `${ROOT}/src`
+const outputPath = `${ROOT}/dist`
+
+const production = process.env.NODE_ENV === 'production'
+
+const config = {
+  mode: process.env.NODE_ENV || 'development',
+  stats: {
+    assets: true,
+    warnings: true,
+    children: false,
+    depth: false,
+    entrypoints: false,
+    errors: true,
+    errorDetails: true,
+    hash: false,
   },
+  entry: ['@babel/polyfill', `${srcPath}/index.js`],
+  output: {
+    path: outputPath,
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+  context: srcPath,
   resolve: {
     alias: {
       react: 'preact/compat',
       'react-dom': 'preact/compat',
-      components: path.resolve(__dirname, 'src/components'),
-    }
+      components: `${srcPath}/components`,
+      assets: `${srcPath}/assets`,
+      lib: `${srcPath}/lib`,
+    },
   },
   module: {
     rules: [
@@ -29,13 +54,68 @@ module.exports = {
           'sass-loader',
         ]
       },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|otf|webp)(\?.*)?$/,
+        loader: 'file-loader',
+        query: {
+          name: 'assets/[name].[ext]'
+        }
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+        query: {
+          name: 'assets/[name].[ext]'
+        }
+      },
+      {
+        test: /\.mp3$/,
+        loader: 'file-loader',
+      },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
-      chunkFilename: '[id].[hash].css',
+    new HtmlWebpackPlugin({
+      title: 'Projec Title',
+      inject: true,
+      template: `${srcPath}/index.ejs`,
+      favicon: `${srcPath}/assets/favicon.ico`,
+      minify: {
+        removeComments: production,
+        collapseWhitespace: production,
+        removeRedundantAttributes: production,
+        useShortDoctype: production,
+        removeEmptyAttributes: production,
+        removeStyleLinkTypeAttributes: production,
+        keepClosingSlash: production,
+        minifyJS: production,
+        minifyCSS: production,
+        minifyURLs: production,
+      },
+      meta: {
+        description: 'Project Description',
+      },
+      cache: false,
     }),
-  ]
+    new MiniCssExtractPlugin({
+      filename: production ? '[name].[hash].css' : '[name].css',
+      chunkFilename: production ? '[id].[hash].css' :'[id].css',
+    }),
+    new CleanWebpackPlugin(),
+    new Dotenv(),
+  ],
 }
+
+if (production) {
+  config.plugins.push(
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css)$/,
+      threshold: 10240,
+      minRatio: 0.7,
+    })
+  )
+}
+
+module.exports = config
